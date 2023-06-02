fa2a14c88cd1eb5e79b393f692edc55fafa7766e
hyerin4405 <mammam4405@gmail.com>
2023-06-02 17:23:23 +0900

    boardDao수정 - 혜린

--- swithMeProject/src/main/java/com/kh/swithme/board/model/dao/BoardDao.java
+++ swithMeProject/src/main/java/com/kh/swithme/board/model/dao/BoardDao.java
@@ -24,8 +24,53 @@ public class BoardDao {
 		RowBounds rowBounds = new RowBounds(offset, pi.getBoardLimit());
 		return (ArrayList)sqlSession.selectList("boardMapper.boardListSelect", boardType, rowBounds);
 	}
+	public int boardCountUp(SqlSessionTemplate sqlSession, int boardNo) {
+		return sqlSession.update("boardMapper.boardCountUp", boardNo);
+	}
 	public int freeCount(SqlSessionTemplate sqlSession) {
 		return sqlSession.selectOne("boardMapper.freeCount");
+	}
+	public Board boardDetail(SqlSessionTemplate sqlSession, int boardNo){
+		return sqlSession.selectOne("boardMapper.boardDetail", boardNo);
+	}
+	public int likeStatus(SqlSessionTemplate sqlSession, int boardNo) {
+		return sqlSession.selectOne("boardMapper.likeStatus", boardNo);
+	}
+	public int likeCount(SqlSessionTemplate sqlSession, int boardNo) {
+		return sqlSession.selectOne("boardMapper.likeCount", boardNo);
+	}
+	public int likeBoard(SqlSessionTemplate sqlSession, int boardNo) {
+		return sqlSession.insert("boardMapper.likeBoard", boardNo);
+	}
+	public int removeLike(SqlSessionTemplate sqlSession, int boardNo) {
+		return sqlSession.delete("boardMapper.removeLike", boardNo);
+	}
+	public int bookStatus(SqlSessionTemplate sqlSession, int boardNo) {
+		return sqlSession.selectOne("boardMapper.bookStatus", boardNo);
+	}
+	public int bookBoard(SqlSessionTemplate sqlSession, int boardNo) {
+		return sqlSession.insert("boardMapper.bookBoard", boardNo);
+	}
+	public int removeBook(SqlSessionTemplate sqlSession, int boardNo) {
+		return sqlSession.delete("boardMapper.removeBook", boardNo);
+	}
+	public int bookCount(SqlSessionTemplate sqlSession, int boardNo) {
+		return sqlSession.selectOne("boardMapper.bookCount", boardNo);
+	}
+	public int replyCount(SqlSessionTemplate sqlSession, int boardNo) {
+		return sqlSession.selectOne("boardMapper.replyCount", boardNo);
+	}
+	public ArrayList<Reply> replyList(SqlSessionTemplate sqlSession, int boardNo){
+		return (ArrayList)sqlSession.selectList("boardMapper.replyList",boardNo);
+	}
+	public ArrayList<ReReply> reReplyList(SqlSessionTemplate sqlSession, int replyNo){
+		return (ArrayList)sqlSession.selectList("boardMapper.reReplyList", replyNo);
+	}
+	public int insertReply(SqlSessionTemplate sqlSession, Reply r) {
+		return sqlSession.insert("boardMapper.insertReply", r);
+	}
+	public int reReplyBoard(SqlSessionTemplate sqlSession, ReReply rere) {
+		return sqlSession.insert("boardMapper.reReplyBoard", rere);
 	}
 	
 	
